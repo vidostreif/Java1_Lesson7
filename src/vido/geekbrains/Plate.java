cbb104777a286a16e413943fd4f624d9d9cdc89d
vido <vido1@yandex.ru>
2018-06-13 09:53:39 +0400

    Копирование основы программы

---
+++ src/vido/geekbrains/Plate.java
@@ -0,0 +1,17 @@
+package vido.geekbrains;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+}
